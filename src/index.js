c689959c17a933268bc4393814b2f6bab6ee3dd1
Chris Dalao <chrisdalao@gmail.com>
2019-06-19 13:51:12 -0700

    view is wired up to props and actions but props is coming back undefined - working on reducer now

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+// needed dependancies
+// applyMiddleware from redux
+// thunk from redux-thunk
+// logger from redux-logger
+// rootReducer from ./reducers
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { getCharacters } from "../actions";
 // import actions
 
 class CharacterListView extends React.Component {
@@ -11,9 +12,12 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
+    console.log(this.props);
   }
 
   render() {
+    console.log(this.props);
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
     }
@@ -27,9 +31,13 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => ({
+  error: state.error,
+  fetching: state.fetching
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { getCharacters }
 )(CharacterListView);
